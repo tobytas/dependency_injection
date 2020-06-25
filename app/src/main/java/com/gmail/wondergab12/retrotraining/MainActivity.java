b14a5912567ab52a2027b350cc7c6bbf8b8d24fb
andrey maysak <wondergab12@gmail.com>
2020-06-25 12:44:27 +0300

    initial commit

--- app/src/main/java/com/gmail/wondergab12/retrotraining/MainActivity.java
+++ app/src/main/java/com/gmail/wondergab12/retrotraining/MainActivity.java
@@ -8,11 +8,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import retrofit2.Call;
-import retrofit2.Callback;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 import retrofit2.Response;
 
+@SuppressWarnings("Convert2Lambda")
 public class MainActivity extends AppCompatActivity {
+
+    private Disposable disposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +30,22 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView textView = findViewById(R.id.textView);
         Button button = findViewById(R.id.button);
-        //noinspection Convert2Lambda
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                NetworkService.getInstance()
-                        .getPlaceHolderService()
-                        .getPostWithId(2)
-                        .enqueue(new Callback<PostContainer>() {
+                disposable = Observable.create(new ObservableOnSubscribe<Response<PostContainer>>() {
+                    @Override
+                    public void subscribe(@NonNull ObservableEmitter<Response<PostContainer>> emitter) throws Throwable {
+                        emitter.onNext(NetworkService.getInstance()
+                                .getPlaceHolderService()
+                                .getPostWithId(2)
+                                .execute());
+                    }
+                }).subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Consumer<Response<PostContainer>>() {
                             @Override
-                            public void onResponse(@NonNull Call<PostContainer> call, @NonNull Response<PostContainer> response) {
+                            public void accept(Response<PostContainer> response) {
                                 PostContainer postContainer = response.body();
                                 if (postContainer != null) {
                                     textView.append(postContainer.getUserId() + "\n");
@@ -39,14 +54,22 @@ public class MainActivity extends AppCompatActivity {
                                     textView.append(postContainer.getBody() + "\n");
                                 }
                             }
-
+                        }, new Consumer<Throwable>() {
                             @Override
-                            public void onFailure(@NonNull Call<PostContainer> call, @NonNull Throwable t) {
-                                textView.append("Request error");
-                                t.printStackTrace();
+                            public void accept(Throwable throwable) {
+                                textView.append("Response error");
+                                throwable.printStackTrace();
                             }
                         });
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (disposable != null) {
+            disposable.dispose();
+        }
     }
 }
